c5821439a3c314ff7ab28b0f4882d20891eb0b45
Dalin <dalin.seivewright@gmail.com>
2019-04-25 17:39:05 -0600

    Adding supplemental script to pull down some scripts and fix the java service.

---
+++ supplemental.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Fixes some post install stuff.
+
+wget https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/startPiNaut.sh -P /opt/pi-naut/
+wget https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/stopPiNaut.sh -P /opt/pi-naut/
+
+cat > /etc/systemd/system/pi-naut.service << EOL
+[Unit]
+Description=Java Pi Naut Service
+
+[Service]
+WorkingDirectory=/opt/pi-naut
+ExecStart=startPiNaut.sh
+ExecStop=stopPiNaut.sh
+
+[Install]
+WantedBy=multi-user.target
+EOL
