cc2115225ec268a5eca8d8696e96f50d52716d82
Dalin <dalin.seivewright@gmail.com>
2019-04-25 17:54:36 -0600

    Update supplemental.sh

--- supplemental.sh
+++ supplemental.sh
@@ -3,6 +3,8 @@
 
 wget https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/startPiNaut.sh -P /opt/pi-naut/
 wget https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/stopPiNaut.sh -P /opt/pi-naut/
+chmod +x /opt/pi-naut/stopPiNaut.sh
+chmod +x /opt/pi-naut/startPiNaut.sh
 
 cat > /etc/systemd/system/pi-naut.service << EOL
 [Unit]
@@ -10,8 +12,9 @@ Description=Java Pi Naut Service
 
 [Service]
 WorkingDirectory=/opt/pi-naut
-ExecStart=startPiNaut.sh
-ExecStop=stopPiNaut.sh
+ExecStart=/opt/pi-naut/startPiNaut.sh
+ExecStop=/opt/pi-naut/stopPiNaut.sh
+SuccessExitStatus=143
 
 [Install]
 WantedBy=multi-user.target
