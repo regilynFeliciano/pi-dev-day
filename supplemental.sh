a89ff88071bf8bb88e8f9f07a13857d26eb54d91
Dalin <dalin.seivewright@gmail.com>
2019-04-25 18:12:04 -0600

    Update supplemental.sh

--- supplemental.sh
+++ supplemental.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Fixes some post install stuff.
 
-sudo wget -O startPiNaut.sh https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/startPiNaut.sh -P /opt/pi-naut/
-sudo wget -O stopPiNaut.sh https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/stopPiNaut.sh -P /opt/pi-naut/
+sudo wget -O /opt/pi-naut/startPiNaut.sh https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/startPiNaut.sh
+sudo wget -O /opt/pi-naut/stopPiNaut.sh https://raw.githubusercontent.com/jtoplak/pi-dev-day/master/stopPiNaut.sh
 sudo chmod +x /opt/pi-naut/stopPiNaut.sh
 sudo chmod +x /opt/pi-naut/startPiNaut.sh
 
